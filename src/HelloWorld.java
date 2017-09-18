37a1c4d2822088782d4149d637b2c61d0769f8e0
Adaamu984 <adam.carr@ocsbstudent.ca>
2017-09-18 13:14:55 -0400

    One.1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Goodbye, I'm Adam");
 		System.out.println("This is my first program");
 		System.out.println("Isn't this fun?");
 		System.out.println("He said \"Do this now!\"");
